76aabd7af1147b98060fd4cbd7341ed372df09af
polinanovikova839-dev <polinanovikova839@gmail.com>
2026-05-07 17:00:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = 'polina-v1';
-const FILES = ['/', '/index.html', '/manifest.json', '/icon.svg'];
-
+const CACHE = 'polina-v2';
+const FILES = ['/', '/index.html', '/manifest.json', '/apple-touch-icon.png', '/icon-192.png', '/icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
 });
-
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
